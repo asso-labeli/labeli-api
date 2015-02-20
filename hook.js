77063bb878fe8ca943c4b3f25b2ac85dba4a171a
AamuLumi <flokauder@hotmail.com>
2015-02-20 17:10:26 +0100

    Update hook.js -> New repo owner and listen on 9001 port

--- hook.js
+++ hook.js
@@ -2,10 +2,8 @@ var gith = require('gith').create();
 
 var execFile = require('child_process').execFile;
 
-gith({repo: 'eolhing/labeli-api'}).on('all', function(payload)
+gith({repo: 'asso-labeli/labeli-api'}).on('all', function(payload)
 {
     if(payload.branch === 'master')
         execFile('./hook.sh');
 });
-
-module.exports = gith;
