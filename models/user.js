b00f717720be0823c62db73cc31837f131b4086f
AamuLumi <flokauder@hotmail.com>
2015-02-23 18:26:00 +0100

    Set defaults value to fields

--- models/user.js
+++ models/user.js
@@ -9,13 +9,13 @@ var UserSchema   = new Schema(
     email : String,
     passwordHash : String,
     privateKey : String,
-    picture : String,
-    level : String,
-    description : String,
-    created : Date,
-    birthday : Date,
-    universityGroup : String,
-    website : String,
+    picture : {type : String, default : null},
+    level : {type : String, default : 1},
+    description : {type : String, default : ''},
+    created : {type : Date, default : Date.now},
+    birthday : {type : Date, default : new Date(1970,1,1)},
+    universityGroup : {type : String, default : 'Inconnu'},
+    website : {type : String, default : null}
 });
 
 module.exports = mongoose.model('User', UserSchema);
