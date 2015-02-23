09b591ed8f70c29f7c040681511442d4b96d6732
AamuLumi <flokauder@hotmail.com>
2015-02-23 19:32:33 +0100

    Correction of birthday date format

--- models/user.js
+++ models/user.js
@@ -13,7 +13,7 @@ var UserSchema   = new Schema(
     level : {type : Number, default : 1},
     description : {type : String, default : ''},
     created : {type : Date, default : Date.now},
-    birthday : {type : Date, default : new Date(1970,1,1)},
+    birthday : {type : Date, default : new Date('01.01.1970T01:00')},
     universityGroup : {type : String, default : 'Inconnu'},
     website : {type : String, default : null}
 });
