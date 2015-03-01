dffb4646246d260977ee3d4e28c46e4af2640897
AamuLumi <flokauder@hotmail.com>
2015-03-01 16:21:08 +0100

    Add Vote Model

---
+++ models/vote.js
@@ -0,0 +1,13 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var VoteSchema   = new Schema(
+{
+    value : Number,
+    created : {type : Date, default : Date.now},
+    lastEdited : {type : Date, default : Date.now},
+    thread : {type: mongoose.Schema.Types.ObjectId, ref: 'Project'},
+    author : {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+});
+
+module.exports = mongoose.model('Vote', VoteSchema);
