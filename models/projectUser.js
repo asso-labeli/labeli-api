93af7080864d5af92dba9ebd6250cdbfa5971cf3
AamuLumi <flokauder@hotmail.com>
2015-03-02 13:41:18 +0100

    Add ProjectUser model

---
+++ models/projectUser.js
@@ -0,0 +1,19 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+/**
+* Level
+* 0 : Membre
+* 1 : Administrateur
+* 2 : Créateur
+**/
+
+var ProjectUserSchema   = new Schema(
+{
+    created : {type : Date, default : Date.now},
+    level : {type : Number, default : 0},
+    author : {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    thread : {type : mongoose.Schema.Types.ObjectId, ref: 'Project'}
+});
+
+module.exports = mongoose.model('ProjectUser', ProjectUserSchema);
