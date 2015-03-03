4748675f445e2f4b1a5bf2f4406e1b12c1d564cc
AamuLumi <flokauder@hotmail.com>
2015-03-03 15:04:29 +0100

    Add ProjectUsers#createOrEditProjectUser()

--- routes/projectUsers.js
+++ routes/projectUsers.js
@@ -1,21 +1,83 @@
 var Project = require('../models/project');
 var User = require('../models/user');
+var ProjectUser = require('../models/projectUser');
 var express = require('express');
 var async = require('async');
 var calls = [];
 
 var router = express.Router();
 
-router.route('/projectUsers/:project_id').post(createProjectUser);
+router.route('/projectUsers/:project_id').post(createOrEditProjectUser);
 router.route('/projectUsers/:project_id').get(getProjectUsers);
 router.route('/projectUser/:projectUser_id').get(getProjectUser);
-router.route('/projectUser/:projectUser_id').put(editProjectUser);
 router.route('/projectUser/:projectUser_id').delete(deleteProjectUser);
 
 module.exports = router;
 
-function createProjectUser(req, res){
+function createOrEditProjectUser(req, res){
+    var projectUser = new ProjectUser();
     
+    var userFound = true;
+    var projectFound = true;
+    
+    if (!('value' in req.body)){
+        res.send({message : "Error : No value given !"});
+        return;
+    }
+    else
+        projectUser.value = req.body.value;
+    
+    if (!("project_id" in req.params)){
+        res.json({message : "Error : No project id given in parameters !"});
+        return ;
+    } else {
+        calls.push(function(callback){
+            Project.findById(req.params.project_id, function(err, project){
+                if (err || project == null) projectFound = false;
+                else projectUser.thread = project;
+                callback();
+            });
+        });   
+    }
+    
+    if (!("authorUsername" in req.body)){
+        res.json({message : "Error : No authorUsername given !"});
+        return ;
+    }
+    else {
+        calls.push(function(callback){
+            User.findOne({username : req.body.authorUsername.toLowerCase()}, function(err, user){
+                if (err || user == null) userFound = false;
+                else projectUser.author = user;
+                callback();
+            });
+        });
+    }
+        
+    async.parallel(calls, function(){
+        if (!projectFound) res.send({message : "Error : Project not found !"});
+        else if (!userFound) res.send({message : "Error : User not found !"});
+        else {
+            ProjectUser.findOne({author : projectUser.author,
+                                 thread : projectUser.thread},
+                                function(err, pu){
+                if (err || pu == null) {        
+                    projectUser.save(function(err){
+                        if (err) res.send(err);
+                        else res.json({ message: 'ProjectUser created !' });
+                    });
+                }
+                else {
+                    pu.value = projectUser.value;
+                    pu.save(function(err){
+                        if (err) res.send(err);
+                        else res.json({message : 'ProjectUser updated !'});
+                    });
+                }
+            });
+        }
+    });
+        
 }
 
 function getProjectUsers(req, res){
