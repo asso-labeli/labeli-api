8c4a121b75bc22519a4a1dbce2fb96172655cf03
AamuLumi <flokauder@hotmail.com>
2015-03-06 13:52:40 +0100

    Remove '!' from messages

--- routes/users.js
+++ routes/users.js
@@ -17,22 +17,22 @@ function createUser(req, res)
 {        
     var user = new User();
 
-    if (!("firstName" in req.body)){
-        Response(res, "Error : No firstName given !", null, 0);
+    if (("firstName" in req.body)){
+        Response(res, "Error : No firstName given", null, 0);
         return ;
     }
     else
         user.firstName = req.body.firstName;
     
-    if (!("lastName" in req.body)){
-        Response(res, "Error : No lastName given !", null, 0);
+    if (("lastName" in req.body)){
+        Response(res, "Error : No lastName given", null, 0);
         return ;
     }
     else
         user.lastName = req.body.lastName;
     
-    if (!("email" in req.body)){
-        Response(res, "Error : No email given !", null, 0);
+    if (("email" in req.body)){
+        Response(res, "Error : No email given", null, 0);
         return ;
     }
     else
@@ -45,7 +45,7 @@ function createUser(req, res)
     user.save(function(err)
     {
         if (err) Response(res, "Error", err, 0);
-        else Response(res, 'User created!', user, 1);
+        else Response(res, 'User created', user, 1);
     });
 }
 
@@ -54,7 +54,7 @@ function getUsers(req, res)
     User.find(function(err, users)
     {
         if (err) Response(res, "Error", err, 0);
-        else Response(res, "Users found!", users, 1);
+        else Response(res, "Users found", users, 1);
     });
 }
 
@@ -64,15 +64,15 @@ function getUser(req, res)
         User.findById(req.params.user_id, function(err, user){
             if (err) Response(res, "Error", err, 0);
             else if (user == null) 
-                Response(res, 'Error : User not found!', null, 0);
-            else Response(res, 'User found!', user, 1);
+                Response(res, 'Error : User not found', null, 0);
+            else Response(res, 'User found', user, 1);
         });
     } else {
         User.findOne({username : req.params.user_id}, function(err, user){
             if (err) Response(res, "Error", err, 0});
             else if (user == null) 
-                Response(res, 'Error : User not found!', null, 0);
-            else Response(res, 'User found!', user, 1);
+                Response(res, 'Error : User not found', null, 0);
+            else Response(res, 'User found', user, 1);
         });
     }        
 }
@@ -81,7 +81,7 @@ function editUser(req, res)
 {
     User.findOne(req.params.user_id, function(err, user)
     {
-        if (err) Response(res, "Error!", err, 0);
+        if (err) Response(res, "Error", err, 0);
         else {
             if ("firstName" in req.body) user.firstName = req.body.firstName;
             if ("lastName" in req.body) user.lastName = req.body.lastName;
@@ -96,7 +96,7 @@ function editUser(req, res)
             user.save(function(err)
             {
                 if (err) Response(res, "Error", err, 0);
-                else Response(res, 'User updated !', user, 1);
+                else Response(res, 'User updated', user, 1);
             });
         }
     });
@@ -106,7 +106,7 @@ function deleteUser(req, res)
     User.remove({_id: req.params.user_id}, function(err, user)
     {
         if (err) Response(res, "Error", err, 0});
-        else Response(res, 'User deleted!', user, 1);
+        else Response(res, 'User deleted', user, 1);
     });
 }
 
