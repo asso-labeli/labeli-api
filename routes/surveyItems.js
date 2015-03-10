ff27c67d963684763f83f3a955df555af4919c82
AamuLumi <flokauder@hotmail.com>
2015-03-10 19:26:44 +0100

    Add SurveyItems functions

--- routes/surveyItems.js
+++ routes/surveyItems.js
@@ -17,21 +17,67 @@ router.route('/surveyItem/:surveyItem_id').delete(deleteSurveyItem);
 module.exports = router;
 
 function createSurveyItem(req, res){
+    var surveyItem = new SurveyItem();
+    var surveyFound = true;
     
+    if (!('name' in req.body)){
+        Response(res, "Error : No name given", null, 0);
+        return;
+    }
+    else
+        surveyItem.name = req.body.name;
+        
+    calls.push(function(callback){
+        Survey.findById(req.params.survey_id, function(err, survey){
+            if (err || survey == null) surveyFound = false;
+            else surveyItem.survey = survey._id;
+            callback();
+        });
+    });
+    
+    async.parallel(calls, function(){
+        if(!surveyFound) Response(res, "Error : Survey not found", null, 0);
+        else surveyItem.save(function(err){
+            if (err) Response(res, "Error", err, 0);
+            else Response(res, "SurveyItem created", surveyItem, 1);
+        });
+    });
 }
 
 function getSurveyItems(req, res){
-    
+    SurveyItem.find({survey : req.params.survey_id}, function(err, sis){
+        if (err) Response(res, "Error", err, 0);
+        else Response(res, "SurveyItems found", sis, 1);
+    });
 }
 
 function getSurveyItem(req, res){
-    
+    SurveyItem.findById(req.params.surveyItem_id, function(err, si){
+        if (err) Response(res, "Error", err, 0);
+        else if (si == null) Response(res, "Error : SurveyItem not found", null, 0);
+        else Response(res, "SurveyItem found", si, 1);
+    });
 }
 
 function editSurveyItem(req, res){
-    
+    SurveyItem.findById(req.params.surveyItem_id, function(err, si){
+        if (err){
+            Response(res, "Error", err, 0);
+            return;
+        }
+        
+        if ('name' in req.body) si.name = req.body.name;
+        
+        si.save(function(err){
+            if (err) Response(res, "Error", err, 0);
+            else Response(res, "SurveyItem updated", si, 1);
+        });
+    });
 }
 
 function deleteSurveyItem(req, res){
-    
+    SurveyItem.remove({_id : req.params.surveyItem_id}, function(err, si){
+        if (err) Response(res, "Error", err, 0);
+        else Response(res, "SurveyItem deleted", si, 1);
+    });
 }
