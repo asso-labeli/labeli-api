ca5eb5a53cbfc8cd085b529323d767d4742f1941
AamuLumi <flokauder@hotmail.com>
2015-03-10 17:10:28 +0100

    Add Survey#isClosed field

--- models/survey.js
+++ models/survey.js
@@ -5,6 +5,7 @@ var SurveySchema   = new Schema(
 {
     description : String,
     name : String,
+    isClosed : {type : Number, default : 0},
     created : {type : Date, default : Date.now},
     lastEdited : {type : Date, default : Date.now},
     author : {type: mongoose.Schema.Types.ObjectId, ref : "User"}
