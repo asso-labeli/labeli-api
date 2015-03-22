fa6b619390fcf41204d6c826033de7682d066976
AamuLumi <flokauder@hotmail.com>
2015-03-22 15:14:52 +0100

    Add comments and JSDOC

--- routes/messages.js
+++ routes/messages.js
@@ -1,3 +1,15 @@
+/**
+ * <table><tr>
+ * <td>POST /messages/:project_id</td><td>{@link Message.createMessage}</td></tr>
+ * <td>GET /messages/:project_id</td><td>{@link Message.getMessages}</td></tr>
+ * <td>GET /messages/:message_id</td><td>{@link Message.getMessage}</td></tr>
+ * <td>PUT /messages/:message_id</td><td>{@link Message.editMessage}</td></tr>
+ * <td>DELETE /messages/:message_id</td><td>{@link Message.deleteMessage}</td></tr>
+ * </table>
+ * @namespace Message
+ * @author Florian Kauder
+ */
+
 var Project = require('../models/project');
 var User = require('../models/user');
 var Message = require('../models/message');
@@ -17,54 +29,59 @@ router.route('/message/:message_id').delete(deleteMessage);
 
 module.exports = router;
 
-function createMessage(req, res){        
+/**
+ * Create a new message
+ * @memberof Message
+ * @param {Express.Request} req - request send
+ * @param {String} req.body.content - content of message
+ * @param {String} req.body.authorUsername - username of author
+ * @param {ObjectID} req.params.project_id - id of project
+ * @param {Express.Response} res - variable to send the response
+ */
+function createMessage(req, res) {
     var message = new Message();
-    
+
     var projectFound = true;
     var userFound = true;
-    
-    if (!("content" in req.body)){
+
+    // Check if variables are send
+    if (!("content" in req.body)) {
         Response(res, "Error : No content given", null, 0);
-        return ;
-    } 
-    else
-        message.content = req.body.content;
-    
-    if (!("project_id" in req.params)){
-        Response(res, "Error : No project id given in parameters", null, 0);
-        return ;
-    } 
-    else {
-        calls.push(function(callback){
-            Project.findById(req.params.project_id, function(err, project){
-                if (err || project == null) projectFound = false;
-                else message.project = project._id;
-                callback();
-            });
-        });   
-    }
-    
-    if (!("authorUsername" in req.body)){
+        return;
+    } else if (!("authorUsername" in req.body)) {
         Response(res, "Error : No authorUsername given", null, 0);
-        return ;
+        return;
     }
-    else {
-        calls.push(function(callback){
-            User.findOne({username : req.body.authorUsername.toLowerCase()}, 
-                         function(err, user){
+
+    // Setting message fields
+    message.content = req.body.content;
+
+    calls.push(function getProject(callback) {
+        Project.findById(req.params.project_id, function (err, project) {
+            if (err || project == null) projectFound = false;
+            else message.project = project._id;
+            callback();
+        });
+    });
+
+    calls.push(function getUserWithUsername(callback) {
+        User.findOne({
+                username: req.body.authorUsername.toLowerCase()
+            },
+            function (err, user) {
                 if (err || user == null) userFound = false;
                 else message.author = user._id;
                 callback();
             });
-        });
-    }
-        
-    async.parallel(calls, function(){
+    });
+
+    // Wait response, and send result
+    async.parallel(calls, function () {
         if (!projectFound) Response(res, "Error : Project not found", null, 0);
         else if (!userFound) Response(res, "Error : User not found", null, 0);
         else {
-            message.save(function(err){
-                if (err) 
+            message.save(function (err) {
+                if (err)
                     Response(res, "Error", err, 0);
                 else Response(res, 'Message created', message, 1);
             });
@@ -72,34 +89,60 @@ function createMessage(req, res){
     });
 }
 
-function getMessages(req, res){
-    Message.find({thread : req.params.project_id}, function(err, messages)
-    {
+
+/**
+ * Get all messages from a project
+ * @memberof Message
+ * @param {Express.Request} req - request send
+ * @param {ObjectID} req.params.project_id - id of project
+ * @param {Express.Response} res - variable to send the response
+ */
+function getMessages(req, res) {
+    Message.find({
+        thread: req.params.project_id
+    }, function (err, messages) {
         if (err) Response(res, "Error", err, 0);
         else Response(res, "Messages found", messages, 1);
     });
 }
-    
-function getMessage(req, res){
-    Message.findById(req.params.message_id, function(err, message){
+
+/**
+ * Get a specific message
+ * @memberof Message
+ * @param {Express.Request} req - request send
+ * @param {ObjectID} req.params.message_id - id of message
+ * @param {Express.Response} res - variable to send the response
+ */
+function getMessage(req, res) {
+    Message.findById(req.params.message_id, function (err, message) {
         if (err) Response(res, "Error", err, 0);
-        else if (message == null) 
+        else if (message == null)
             Response(res, "Error : Message not found", message, 0);
         else Response(res, "Message found", message, 1);
     });
 }
-    
-function editMessage(req, res){
-    Message.findById(req.params.message_id, function(err, message)
-    {
+
+/**
+ * Edit a message
+ * @memberof Message
+ * @param {Express.Request} req - request send
+ * @param {ObjectID} req.params.message_id - id of message
+ * @param {String} [req.body.content] - new content
+ * @param {Express.Response} res - variable to send the response
+ */
+function editMessage(req, res) {
+    Message.findById(req.params.message_id, function (err, message) {
         if (err) {
             Response(res, "Error", err, 0);
             return;
         }
-        if ("content" in req.body) message.content = req.body.content;
-        message.lastEdited = Date.now();
         
-        message.save(function(err){
+        // Edit content if exists in request
+        if ("content" in req.body) message.content = req.body.content;
+        // Edit the lastEdited time
+        message.lastEdited = Date.now();
+
+        message.save(function (err) {
             if (err) Response(res, "Error", err, 0);
             else Response(res, 'Message edited', message, 1);
         });
@@ -107,9 +150,17 @@ function editMessage(req, res){
     });
 }
 
-function deleteMessage(req, res){
-    Message.remove({_id: req.params.message_id}, function(err, obj)
-    {
+/**
+ * Delete a message
+ * @memberof Message
+ * @param {Express.Request} req - request send
+ * @param {ObjectID} req.params.message_id - id of message
+ * @param {Express.Response} res - variable to send the response
+ */
+function deleteMessage(req, res) {
+    Message.remove({
+        _id: req.params.message_id
+    }, function (err, obj) {
         if (err) Response(res, "Error", err, 0);
         else Response(res, 'Message deleted', obj, 1);
     });
