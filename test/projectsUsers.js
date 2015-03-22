ec262125a54094cf45490ecc65712007684df2b5
AamuLumi <flokauder@hotmail.com>
2015-03-22 23:24:57 +0100

    Add authentification tests in ProjectUser module

--- test/projectsUsers.js
+++ test/projectsUsers.js
@@ -3,20 +3,55 @@ var expect = require('chai').expect,
     should = require('chai').should();
 
 var request = require('supertest');
+var superagent = require('superagent');
+var agent = superagent.agent();
+var agent2 = superagent.agent();
+var agentAdmin = superagent.agent();
 
 var apiUrl = 'http://localhost:9010';
 
 var projectTest = null;
 var userTest = null;
+var user2Test = null;
+var adminTest = null;
 var projectUserTest = null;
 var projectUserValue = null;
+var projectUserTest2 = null;
+var projectUserTest3 = null;
     
 describe('ProjectUser', function(){
     describe('Preparation', function(){
-        it('must create a new user', function(done){
+        it('must create a new admin', function (done) {
             request(apiUrl)
-            .post('/users')
-            .send({firstName : 'projectUserTest',
+                .post('/admin')
+                .end(function (err, res) {
+                    if (err) return err;
+                    expect(res.body.success).to.equal(1);
+                    adminTest = res.body.data._id;
+                    done();
+                });
+        });
+
+        it('must logged the admin', function (done) {
+            request(apiUrl)
+                .post('/auth')
+                .send({
+                    username: "admintest.admintest",
+                    password: '098f6bcd4621d373cade4e832627b4f6'
+                })
+                .end(function (err, res) {
+                    if (err) return err;
+                    expect(res.body.success).to.equal(1);
+                    expect(res.body.message).to.equal("Authentification successfull");
+                    agentAdmin.saveCookies(res);
+                    done();
+                });
+        });
+        
+        it('must create a new user', function(done){
+            var req = request(apiUrl).post('/users');
+            agentAdmin.attachCookies(req);
+            req.send({firstName : 'projectUserTest',
                   lastName : 'projectUserTest',
                   email : 'something@email.com'})
             .end(function(err, res){
@@ -26,11 +61,57 @@ describe('ProjectUser', function(){
                 done();
             });
         });
+        
+        it('must logged the test user', function (done) {
+            request(apiUrl)
+                .post('/auth')
+                .send({
+                    username: "projectusertest.projectusertest",
+                    password: '098f6bcd4621d373cade4e832627b4f6'
+                })
+                .end(function (err, res) {
+                    if (err) return err;
+                    expect(res.body.success).to.equal(1);
+                    expect(res.body.message).to.equal("Authentification successfull");
+                    agent.saveCookies(res);
+                    done();
+                });
+        });
+        
+        it('must create a second new user', function(done){
+            var req = request(apiUrl).post('/users');
+            agentAdmin.attachCookies(req);
+            req.send({firstName : 'projectUserTestSecond',
+                  lastName : 'projectUserTestSecond',
+                  email : 'something@email.com'})
+            .end(function(err, res){
+                if (err) return err;
+                expect(res.body.success).to.equal(1);
+                userTest = res.body.data._id;
+                done();
+            });
+        });
+        
+        it('must logged the second test user', function (done) {
+            request(apiUrl)
+                .post('/auth')
+                .send({
+                    username: "projectusertestsecond.projectusertestsecond",
+                    password: '098f6bcd4621d373cade4e832627b4f6'
+                })
+                .end(function (err, res) {
+                    if (err) return err;
+                    expect(res.body.success).to.equal(1);
+                    expect(res.body.message).to.equal("Authentification successfull");
+                    agent2.saveCookies(res);
+                    done();
+                });
+        });
 
         it('must create a new project', function(done){
-            request(apiUrl)
-            .post('/projects')
-            .send({name : "ProjectUser Module Test",
+            var req = request(apiUrl).post('/projects');
+            agentAdmin.attachCookies(req);
+            req.send({name : "ProjectUser Module Test",
                    type : 0,
                    authorUsername : "projectUsertest.projectUsertest"})
             .end(function(err, res){
@@ -43,10 +124,21 @@ describe('ProjectUser', function(){
     });
                
     describe('.createOrEditProjectUser()', function(){
-        it('must need a value', function(done){
+        it('must need to be logged', function(done){
             request(apiUrl)
             .post('/projectUsers/42')
             .end(function(err, res){
+                if (err) return done(err);
+                expect(res.body.message).to.equal("Error : Not logged");
+                expect(res.body.success).to.equal(0);
+                done();
+            });
+        });
+        
+        it('must need a value', function(done){
+            var req = request(apiUrl).post('/projectUsers/42');
+            agent2.attachCookies(req);
+            req.end(function(err, res){
                 if (err) return done(err);
                 expect(res.body.message).to.equal("Error : No value given");
                 expect(res.body.success).to.equal(0);
@@ -54,46 +146,22 @@ describe('ProjectUser', function(){
             });
         });
         
-        it('must need an author username', function(done){
-            request(apiUrl)
-            .post('/projectUsers/42')
-            .send({value : 1})
-            .end(function(err, res){
-                if (err) return done(err);
-                expect(res.body.message).to.equal("Error : No authorUsername given");
-                expect(res.body.success).to.equal(0);
-                done();
-            });
-        });
-        
-        it('must check the project given', function(done){
-            request(apiUrl)
-            .post('/projectUsers/42')
-            .send({value : 1, authorUsername : "a"})
+        it ('simple member cannot add a new administrator to project', function(done){
+            var req = request(apiUrl).post('/projectUsers/'+projectTest);
+            agent2.attachCookies(req);
+            req.send({value : 0, username : "admintest.admintest"})
             .end(function(err, res){
                 if (err) return done(err);
                 expect(res.body.success).to.equal(0);
-                expect(res.body.message).to.equal("Error : Project not found");
-                done();
-            });
-        });
-        
-        it('must check the user given', function(done){
-            request(apiUrl)
-            .post('/projectUsers/'+projectTest)
-            .send({value : 1, authorUsername : "a"})
-            .end(function(err, res){
-                if (err) return done(err);
-                expect(res.body.success).to.equal(0);
-                expect(res.body.message).to.equal("Error : User not found");
+                expect(res.body.message).to.equal("Error : You're not an admin");
                 done();
             });
         });
         
         it ('must create a new ProjectUser with good informations', function(done){
-            request(apiUrl)
-            .post('/projectUsers/'+projectTest)
-            .send({value : 1, authorUsername : "projectUsertest.projectUsertest"})
+            var req = request(apiUrl).post('/projectUsers/'+projectTest);
+            agent2.attachCookies(req);
+            req.send({value : 0})
             .end(function(err, res){
                 if (err) return done(err);
                 expect(res.body.success).to.equal(1);
@@ -106,18 +174,32 @@ describe('ProjectUser', function(){
             });
         });
         
-        it('must edit the value', function(done){
-            request(apiUrl)
-            .post('/projectUsers/'+projectTest)
-            .send({value : -1, authorUsername : "projectUsertest.projectUsertest"})
+        it('creator can add a new administrator to his project', function(done){
+            var req = request(apiUrl).post('/projectUsers/'+projectTest);
+            agent.attachCookies(req);
+            req.send({value : 1, username : "projectUsertestsecond.projectUsertestsecond"})
             .end(function(err, res){
                 if (err) return done(err);
                 expect(res.body.success).to.equal(1);
-                expect(res.body.data.value).to.equal(-1);
+                expect(res.body.data.value).to.equal(1);
                 expect(res.body.data.project).to.equal(projectTest);
-                expect(res.body.data.author).to.equal(userTest);
-                projectUserTest = res.body.data._id;
-                projectUserValue = res.body.data.value;
+                expect(res.body.data.author).to.equal(user2Test);
+                projectUserTest2 = res.body.data._id;
+                done();
+            });
+        });
+        
+        it('administrator can add a new administrator to his project', function(done){
+            var req = request(apiUrl).post('/projectUsers/'+projectTest);
+            agent2.attachCookies(req);
+            req.send({value : 1, username : "admintest.admintest"})
+            .end(function(err, res){
+                if (err) return done(err);
+                expect(res.body.success).to.equal(1);
+                expect(res.body.data.value).to.equal(1);
+                expect(res.body.data.project).to.equal(projectTest);
+                expect(res.body.data.author).to.equal(adminTest);
+                projectUserTest2 = res.body.data._id;
                 done();
             });
         });
@@ -149,32 +231,113 @@ describe('ProjectUser', function(){
     });
 
     describe('.deleteProjectUser()', function(){
-        it('must delete the testProjectUser', function(done){
-            request(apiUrl)
-            .delete('/projectUser/' + projectUserTest)
-            .end(function(err, res){
-                if (err) return done(err);
-                expect(res.body.success).to.equal(1);
-                done();
-            });
-        });
-
-        it('must be deleted', function(done){
-            request(apiUrl)
-            .get('/projectUser/' + projectUserTest)
+        it('administrator cannot kick the creator', function(done){
+            var req = request(apiUrl).delete('/projectUser/' + projectUserTest);
+            agent2.attachCookies(req);
+            req.request(apiUrl)
             .end(function(err, res){
                 if (err) return done(err);
                 expect(res.body.success).to.equal(0);
+                expect(res.body.message).to.equal("Error : You cannot kick the creator");
+                done();
+            });
+        });
+        
+        it('administrator can kick an administrator and members', function(done){
+            var req = request(apiUrl).delete('/projectUser/' + projectUserTest3);
+            agent2.attachCookies(req);
+            req.request(apiUrl)
+            .end(function(err, res){
+                if (err) return done(err);
+                expect(res.body.success).to.equal(1);
+                expect(res.body.message).to.equal("ProjectUser deleted");
+                done();
+            });
+        });
+        
+        it('creator can kick everyone', function(done){
+            var req = request(apiUrl).delete('/projectUser/' + projectUserTest2);
+            agent.attachCookies(req);
+            req.request(apiUrl)
+            .end(function(err, res){
+                if (err) return done(err);
+                expect(res.body.success).to.equal(1);
+                expect(res.body.message).to.equal("ProjectUser deleted");
+                done();
+            });
+        });
+        
+        it('creator cannot kick himself', function(done){
+            var req = request(apiUrl).delete('/projectUser/' + projectUserTest);
+            agent.attachCookies(req);
+            req.request(apiUrl)
+            .end(function(err, res){
+                if (err) return done(err);
+                expect(res.body.success).to.equal(1);
+                expect(res.body.message).to.equal("ProjectUser deleted");
                 done();
             });
         });
     });
     
     describe('End of test', function(){
-        it ('must delete the user test', function(done){
-            request(apiUrl)
-            .delete('/users/'+userTest)
-            .end(function(err, res){
+        it('must logout the userTest', function (done) {
+            var req = request(apiUrl).delete('/auth')
+            agent.attachCookies(req);
+            req.end(function (err, res) {
+                if (err) return err;
+                expect(res.body.success).to.equal(1);
+                done();
+            });
+        });
+
+        it('must delete the test user', function (done) {
+            var req = request(apiUrl)
+                .delete('/users/' + userTest);
+            agentAdmin.attachCookies(req);
+            req.end(function (err, res) {
+                if (err) return done(err);
+                expect(res.body.success).to.equal(1);
+                done();
+            });
+        });
+
+        it('must logout the user2Test', function (done) {
+            var req = request(apiUrl).delete('/auth')
+            agent2.attachCookies(req);
+            req.end(function (err, res) {
+                if (err) return err;
+                expect(res.body.success).to.equal(1);
+                done();
+            });
+        });
+
+        it('must delete the user2Test', function (done) {
+            var req = request(apiUrl)
+                .delete('/users/' + user2Test);
+            agentAdmin.attachCookies(req);
+            req.end(function (err, res) {
+                if (err) return done(err);
+                expect(res.body.success).to.equal(1);
+                done();
+            });
+        });
+
+        it('must delete the adminTest', function (done) {
+            var req = request(apiUrl)
+                .delete('/users/' + adminTest);
+            agentAdmin.attachCookies(req);
+            req.end(function (err, res) {
+                if (err) return done(err);
+                expect(res.body.success).to.equal(1);
+                done();
+            });
+        });
+
+        it('must logout the adminTest', function (done) {
+            var req = request(apiUrl).delete('/auth')
+            agentAdmin.attachCookies(req);
+            req.end(function (err, res) {
                 if (err) return err;
                 expect(res.body.success).to.equal(1);
                 done();
@@ -182,11 +345,21 @@ describe('ProjectUser', function(){
         });
         
         it ('must delete the project test', function(done){
-            request(apiUrl)
-            .delete('/projects/'+projectTest)
-            .end(function(err, res){
+            var req = request(apiUrl).delete('/projects/'+projectTest);
+            agentAdmin.attachCookies(req);
+            req.end(function(err, res){
                 if (err) return err;
                 expect(res.body.success).to.equal(1);
+                done();
+            });
+        });
+        
+        it('must be deleted', function(done){
+            request(apiUrl)
+            .get('/projectUser/' + projectUserTest)
+            .end(function(err, res){
+                if (err) return done(err);
+                expect(res.body.success).to.equal(0);
                 done();
             });
         });
