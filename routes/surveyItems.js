732ffb30a81c997bc4a1c09fbe384b05b192a44c
AamuLumi <flokauder@hotmail.com>
2015-03-23 16:29:50 +0100

    Add JSDOC of SurveyItem module

--- routes/surveyItems.js
+++ routes/surveyItems.js
@@ -1,3 +1,23 @@
+/**
+ * <h2>Model</h2>
+ * <table>
+ * <tr><td><b>Name</b></td><td><b>Type</b></td><td><b>Default Value</b></td></tr>
+ * <tr><td>name</td><td>String</td></tr>
+ * <tr><td>created</td><td>Date</td><td>Date.now</td></tr>
+ * <tr><td>lastEdited</td><td>Date</td><td>Date.now</td></tr>
+ * <tr><td>survey</td><td>ObjectId</td></tr>
+ * </table><br>
+ * <h2>Routing Table</h2>
+ * <table>
+ * <tr><td>POST /surveyItems/:survey_id</td><td>{@link SurveyItem.createSurveyItem}</td></tr>
+ * <tr><td>GET /surveyItems/:survey_id</td><td>{@link SurveyItem.getSurveyItems}</td></tr>
+ * <tr><td>GET /surveyItem/:surveyItem_id</td><td>{@link SurveyItem.getSurveyItem}</td></tr>
+ * <tr><td>PUT /surveyItem/:surveyItem_id</td><td>{@link SurveyItem.editSurveyItem}</td></tr>
+ * <tr><td>DELETE /surveyItem/:surveyItem_id</td><td>{@link SurveyItem.deleteSurveyItem}</td></tr></table><br>
+ * @namespace SurveyItem
+ * @author Florian Kauder
+ */
+
 var Survey = require('../models/survey');
 var SurveyItem = require('../models/surveyItem');
 var Response = require('../modules/response');
@@ -16,6 +36,15 @@ router.route('/surveyItem/:surveyItem_id').delete(deleteSurveyItem);
 
 module.exports = router;
 
+/**
+ * Create a new surveyItem<br>
+ * <b>Level needed :</b> Owner | Admin
+ * @memberof SurveyItem
+ * @param {Express.Request} req - request send
+ * @param {String} req.body.name - name of the item
+ * @param {ObjectId} req.params.survey_id - ID of survey
+ * @param {Express.Response} res - variable to send the response
+ */
 function createSurveyItem(req, res){
     var surveyItem = new SurveyItem();
     var surveyFound = true;
@@ -44,6 +73,14 @@ function createSurveyItem(req, res){
     });
 }
 
+/**
+ * Get all surveyItems of a survey<br>
+ * <b>Level needed :</b> Guest
+ * @memberof SurveyItem
+ * @param {Express.Request} req - request send
+ * @param {ObjectId} req.params.survey_id - ID of survey
+ * @param {Express.Response} res - variable to send the response
+ */
 function getSurveyItems(req, res){
     SurveyItem.find({survey : req.params.survey_id}, function(err, sis){
         if (err) Response(res, "Error", err, 0);
@@ -51,6 +88,14 @@ function getSurveyItems(req, res){
     });
 }
 
+/**
+ * Get a specific surveyItem<br>
+ * <b>Level needed :</b> Guest
+ * @memberof SurveyItem
+ * @param {Express.Request} req - request send
+ * @param {ObjectId} req.params.surveyItem_id - ID of surveyItem
+ * @param {Express.Response} res - variable to send the response
+ */
 function getSurveyItem(req, res){
     SurveyItem.findById(req.params.surveyItem_id, function(err, si){
         if (err) Response(res, "Error", err, 0);
@@ -59,6 +104,15 @@ function getSurveyItem(req, res){
     });
 }
 
+/**
+ * Edit a surveyItem<br>
+ * <b>Level needed :</b> Owner | Admin
+ * @memberof SurveyItem
+ * @param {Express.Request} req - request send
+ * @param {String} [req.body.name] - new name
+ * @param {ObjectId} req.params.surveyItem_id - ID of surveyItem
+ * @param {Express.Response} res - variable to send the response
+ */
 function editSurveyItem(req, res){
     SurveyItem.findById(req.params.surveyItem_id, function(err, si){
         if (err){
@@ -75,6 +129,14 @@ function editSurveyItem(req, res){
     });
 }
 
+/**
+ * Delete a surveyItem<br>
+ * <b>Level needed :</b> Owner | Admin
+ * @memberof SurveyItem
+ * @param {Express.Request} req - request send
+ * @param {ObjectId} req.params.surveyItem_id - ID of surveyItem
+ * @param {Express.Response} res - variable to send the response
+ */
 function deleteSurveyItem(req, res){
     SurveyItem.remove({_id : req.params.surveyItem_id}, function(err, si){
         if (err) Response(res, "Error", err, 0);
