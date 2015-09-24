76ce33b10e384bd0d1add73468ce452b48eb3065
aamulumi <flokauder@hotmail.com>
2015-09-24 21:11:47 +0200

    Add return values on Survey module

--- routes/surveys.js
+++ routes/surveys.js
@@ -31,6 +31,8 @@ var Survey = require('../models/survey');
 var Response = require('../modules/response');
 var Log = require('../modules/log');
 
+var isMongooseId = require('mongoose').Types.ObjectId.isValid;
+
 var express = require('express');
 var async = require('async');
 var calls = [];
@@ -47,7 +49,17 @@ module.exports = router;
 
 /**
  * Create a new survey<br>
- * <b>Level needed :</b> Member
+ * <b>Level needed :</b> Member<br>
+ * <h5>Return Table:</h5>
+ * <table>
+ * <tr><td><b>Code</b></td><td><b>Value</b></td></tr>
+ * <tr><td>1</td><td>Success</td></tr>
+ * <tr><td>-1</td><td>Not logged</td></tr>
+ * <tr><td>-3</td><td>Not at least a member</td></tr>
+ * <tr><td>-21</td><td>Survey with name already exists</td></tr>
+ * <tr><td>-22</td><td>Survey not found</td></tr>
+ * <tr><td>-29</td><td>MangoDB error during save()</td></tr>
+ * </table>
  * @memberof Survey
  * @param {Express.Request} req - request send
  * @param {String} req.body.name - name of the survey
@@ -56,94 +68,126 @@ module.exports = router;
  * @param {Express.Response} res - variable to send the response
  */
 function createSurvey(req, res) {
-    if (req.session.level == User.Level.Guest) {
-        Response(res, "Error : Not logged", null, 0);
-        return;
-    } else if (req.session.level < User.Level.OldMember) {
-        Response(res, "Error : You're not a member", null, 0);
-        return;
+  if (req.session.level == User.Level.Guest) {
+    Response.notLogged(res);
+    return;
+  }
+  else if (req.session.level < User.Level.Member) {
+    Response.notMember(res);
+    return;
+  }
+
+  var survey = new Survey();
+  var surveyNameFound = false;
+
+  // Check variables in req.body
+  if (!('name' in req.body)) {
+    Response.missing(res, 'name');
+    return;
+  }
+
+  if ('description' in req.body) survey.description = req.body.description;
+  if ('state' in req.body) {
+    if (state > 1) survey.state = 1;
+    else if (state < 0) survey.state = 0;
+    else survey.state = req.body.state;
+  }
+
+  // Assign values to survey
+  survey.name = req.body.name;
+  survey.author = req.session.userId;
+
+  async.parallel([
+    function searchSurveyWithSameName(callback) {
+      Survey.findOne({
+        name: req.body.name
+      }, function afterNameSearch(err, s) {
+        if (err || s == null) surveyNameFound = false;
+        else surveyNameFound = true;
+        callback();
+      });
     }
-
-    var survey = new Survey();
-    var surveyNameFound = false;
-
-    // Check variables in req.body
-    if (!('name' in req.body)) {
-        Response(res, "Error : No name given", null, 0);
-        return;
-    }
-
-    if ('description' in req.body) survey.description = req.body.description;
-    if ('state' in req.body) {
-            if (state > 1) survey.state = 1;
-            else if (state < 0) survey.state = 0;
-            else survey.state = req.body.state;
+  ], function afterSearch() {
+    if (surveyNameFound)
+      Response.alreadyExist(res, 'survey with this name');
+    else
+      survey.save(function afterSurveySave(err) {
+        if (err) Response.saveError(err, res);
+        else {
+          Response.success(res, "Survey created", survey);
+          Log.i("Survey \"" + survey.name + "\"(" + survey._id +
+            ") created by user " + req.session.userId);
         }
-
-    // Assign values to survey
-    survey.name = req.body.name;
-    survey.author = req.session.userId;
-
-    async.parallel([
-        function searchSurveyWithSameName(callback) {
-            Survey.findOne({
-                name: req.body.name
-            }, function (err, s) {
-                if (err || s == null) surveyNameFound = false;
-                else surveyNameFound = true;
-                callback();
-            });
-    }], function () {
-        if (surveyNameFound)
-            Response(res, "Error : Survey's name already exists", null, 0);
-        else
-            survey.save(function (err) {
-                if (err) Response(res, "Error", err, 0);
-                else {
-                    Response(res, "Survey created", survey, 1);
-                    Log.i("Survey \"" + survey.name + "\"(" + survey._id +
-                          ") created by user " + req.session.userId);
-                }
-            });
-    });
+      });
+  });
 }
 
 /**
  * Get all surveys<br>
- * <b>Level needed :</b> Guest
+ * <b>Level needed :</b> Guest<br>
+ * <h5>Return Table:</h5>
+ * <table>
+ * <tr><td><b>Code</b></td><td><b>Value</b></td></tr>
+ * <tr><td>1</td><td>Success</td></tr>
+ * <tr><td>-22</td><td>Survey not found</td></tr>
+ * <tr><td>-27</td><td>MangoDB error during find()</td></tr>
+ * </table>
  * @memberof Survey
  * @param {Express.Request} req - request send
  * @param {Express.Response} res - variable to send the response
  */
 function getSurveys(req, res) {
-    Survey.find(function (err, surveys) {
-        if (err) Response(res, "Error", err, 0);
-        else Response(res, "Surveys found", surveys, 1);
-    });
+  Survey.find(function afterSurveySearch(err, surveys) {
+    if (err) Response.findError(res, err);
+    else if (typeof surveys === 'undefined' || surveys.length == 0)
+      Response.notFound(res, 'survey');
+    else Response.success(res, "Surveys found", surveys);
+  });
 }
 
 /**
  * Get a specific survey<br>
- * <b>Level needed :</b> Guest
+ * <b>Level needed :</b> Guest<br>
+ * <h5>Return Table:</h5>
+ * <table>
+ * <tr><td><b>Code</b></td><td><b>Value</b></td></tr>
+ * <tr><td>1</td><td>Success</td></tr>
+ * <tr><td>-22</td><td>Survey not found</td></tr>
+ * <tr><td>-27</td><td>MangoDB error during find()</td></tr>
+ * <tr><td>-31</td><td>Invalid ID</td></tr>
+ * </table>
  * @memberof Survey
  * @param {Express.Request} req - request send
  * @param {ObjectID} req.params.survey_id - ID of survey
  * @param {Express.Response} res - variable to send the response
  */
 function getSurvey(req, res) {
-    Survey.findById(req.params.survey_id, function (err, survey) {
-        if (err)
-            Response(res, "Error", err, 0);
-        else if (survey == null)
-            Response(res, "Error : Survey not found", null, 0);
+  if (!isMongooseId(req.params.survey_id))
+    Response.invalidID(res);
+  else
+    Survey.findById(req.params.survey_id,
+      function afterSurveySearch(err, survey) {
+        if (err) Response.findError(res, err);
+        else if (survey == null) Response.notFound(res, 'survey');
         else
-            Response(res, "Survey found", survey, 1);
-    });
+          Response.success(res, "Survey found", survey);
+      });
 }
 
 /**
  * Edit a survey<br>
- * <b>Level needed :</b> Owner | Admin
+ * <b>Level needed :</b> Owner | Admin<br>
+ * <h5>Return Table:</h5>
+ * <table>
+ * <tr><td><b>Code</b></td><td><b>Value</b></td></tr>
+ * <tr><td>1</td><td>Success</td></tr>
+ * <tr><td>-1</td><td>Not logged</td></tr>
+ * <tr><td>-4</td><td>Not owner</td></tr>
+ * <tr><td>-22</td><td>Survey not found</td></tr>
+ * <tr><td>-27</td><td>MangoDB error during find()</td></tr>
+ * <tr><td>-29</td><td>MangoDB error during save()</td></tr>
+ * <tr><td>-31</td><td>Invalid ID</td></tr>
+ * </table>
  * @memberof Survey
  * @param {Express.Request} req - request send
  * @param {String} [req.body.description] - new description
@@ -153,79 +197,88 @@ function getSurvey(req, res) {
  * @param {Express.Response} res - variable to send the response
  */
 function editSurvey(req, res) {
-    if (req.session.level == User.Level.Guest) {
-        Response(res, "Error : Not logged", null, 0);
-        return;
-    }
-
-    Survey.findById(req.params.survey_id, function (err, survey) {
+  if (req.session.level == User.Level.Guest)
+    Response.notLogged(res);
+  else if (!isMongooseId(req.params.survey_id))
+    Response.invalidID(res);
+  else
+    Survey.findById(req.params.survey_id,
+      function afterSurveySearch(err, survey) {
         if (err) {
-            Response(res, "Error", err, 0);
-            return;
-        } else if (survey == null) {
-            Response(res, "Error : Survey not found", null, 0);
-            return;
-        } else if ((survey.author != req.session.userId) && (req.session.level < User.Level.Admin)) {
-            Response(res, "Error : You're not an admin", null, 0);
-            return;
+          Response.findError(res, err);
+          return;
+        }
+        else if (survey == null) {
+          Response.notFound(res, 'survey');
+          return;
+        }
+        else if ((survey.author != req.session.userId) && (req.session.level < User.Level.Admin)) {
+          Response.notOwner(res);
+          return;
         }
 
         // Change values
         if ('description' in req.body) survey.description = req.body.description;
         if ('name' in req.body) survey.name = req.body.name;
         if ('state' in req.body) {
-            if (state > 1) survey.state = 1;
-            else if (state < 0) survey.state = 0;
-            else survey.state = req.body.state;
+          if (state > 1) survey.state = 1;
+          else if (state < 0) survey.state = 0;
+          else survey.state = req.body.state;
         }
 
-        survey.save(function (err) {
-            if (err) Response(res, "Error", err, 0);
-            else {
-                Response(res, "Survey updated", survey, 1);
-                Log.i("Survey \"" + survey.name + "\"(" + survey._id +
-                      ") edited by user " + req.session.userId);
-            }
+        survey.save(function afterSurveySearch(err) {
+          if (err) Response.saveError(res, err);
+          else {
+            Response.success(res, "Survey updated", survey);
+            Log.i("Survey \"" + survey.name + "\"(" + survey._id +
+              ") edited by user " + req.session.userId);
+          }
         });
-    });
+      });
 }
 
 /**
  * Delete a survey<br>
- * <b>Level needed :</b> Owner | Admin
+ * <b>Level needed :</b> Owner | Admin<br>
+ * <h5>Return Table:</h5>
+ * <table>
+ * <tr><td><b>Code</b></td><td><b>Value</b></td></tr>
+ * <tr><td>1</td><td>Success</td></tr>
+ * <tr><td>-1</td><td>Not logged</td></tr>
+ * <tr><td>-4</td><td>Not owner</td></tr>
+ * <tr><td>-22</td><td>Survey not found</td></tr>
+ * <tr><td>-27</td><td>MangoDB error during find()</td></tr>
+ * <tr><td>-28</td><td>MangoDB error during remove()</td></tr>
+ * <tr><td>-31</td><td>Invalid ID</td></tr>
+ * </table>
  * @memberof Survey
  * @param {Express.Request} req - request send
  * @param {ObjectID} req.params.survey_id - ID of survey
  * @param {Express.Response} res - variable to send the response
  */
 function deleteSurvey(req, res) {
-    if (req.session.level == User.Level.Guest) {
-        Response(res, "Error : Not logged", null, 0);
-        return;
-    }
-
-    Survey.findById(req.params.survey_id, function (err, survey) {
-        if (err) {
-            Response(res, "Error", err, 0);
-            return;
-        } else if (survey == null) {
-            Response(res, "Error : Survey not found", null, 0);
-            return;
-        } else if ((survey.author != req.session.userId) && (req.session.level < User.Level.Admin)) {
-            Response(res, "Error : You're not an admin", null, 0);
-            return;
-        }
-
-        Survey.remove({
+  if (req.session.level == User.Level.Guest)
+    Response.notLogged(res);
+  else if (!isMongooseId(req.params.survey_id))
+    Response.invalidID(res);
+  else
+    Survey.findById(req.params.survey_id,
+      function afterSurveySearch(err, survey) {
+        if (err) Response.findError(res, err);
+        else if (survey == null) Response.notFound(res, 'survey');
+        else if ((survey.author != req.session.userId) &&
+          (req.session.level < User.Level.Admin))
+          Response.notOwner(res);
+        else
+          Survey.remove({
             _id: req.params.survey_id
-        }, function (err, survey) {
-            if (err) Response(res, "Error", err, 0);
+          }, function afterSurveyRemove(err, survey) {
+            if (err) Response.removeError(res, err);
             else {
-                Response(res, "Survey deleted", survey, 1);
-                Log.i("Survey \"" + survey.name + "\"(" + survey._id +
-                      ") deleted by user " + req.session.userId);
+              Response.success(res, "Survey deleted", survey);
+              Log.i("Survey \"" + survey.name + "\"(" + survey._id +
+                ") deleted by user " + req.session.userId);
             }
-        });
-    });
-
+          });
+      });
 }
